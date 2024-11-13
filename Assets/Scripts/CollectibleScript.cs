0d511d35b89f96cfc97b9cc965d76c9965c4b912
sohamPatkar <spatkar398@gmail.com>
2024-11-13 17:14:32 +0530

    Added Collectible pickup animation

--- Assets/Scripts/CollectibleScript.cs
+++ Assets/Scripts/CollectibleScript.cs
@@ -1,14 +1,27 @@
+using System.Collections;
 using UnityEngine;
 
 public class CollectibleScript : MonoBehaviour
 {
+    private Animator keyAnimator;
+    private void Start()
+    {
+        keyAnimator = GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<PlayerMovement>() != null)
         {
             PlayerMovement player = other.gameObject.GetComponent<PlayerMovement>();
             player.PickUp();
-            Destroy(gameObject);
+            keyAnimator.Play("Key_Collected");
+            StartCoroutine(KeyCollected());
         }
+    }
+
+    IEnumerator KeyCollected()
+    {
+        yield return new WaitForSeconds(0.7f);
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -1,6 +1,7 @@
 using TMPro;
+using UnityEngine;
 
-public class ScoreController
+public class ScoreController : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
 
